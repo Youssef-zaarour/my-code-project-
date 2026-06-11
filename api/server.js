7bf1796c93b243cc72a1a4c80375e13cedb75928
Youssef-zaarour <euorop@hotmail.com>
2026-06-11 13:10:38 +0300

    Serve HTML files for home, success, and cancel routes
    Updated home route to serve index.html and added success and cancel routes.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Serve static files (like favicon, images, css)
+app.use(express.static(path.join(__dirname, '../')));
 
 // Home route
 app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-// Export the app instead of using app.listen
+// Success route
+app.get('/success', (req, res) => {
+  res.sendFile(path.join(__dirname, '../success.html'));
+});
+
+// Cancel route
+app.get('/cancel', (req, res) => {
+  res.sendFile(path.join(__dirname, '../cancel.html'));
+});
+
+// Export app for Vercel
 module.exports = app;
